300079ea844ef0c32790401833903fa1f2d1afbb
Thiago Nascimento <thiagodnf@gmail.com>
2015-08-15 01:23:09 -0300

    add examples to help the users

---
+++ examples/hom_2nd_order/gt.c
@@ -0,0 +1,6 @@
+int gt(int a, int b){
+	if(a > b && 1 == 1){
+		return a;
+	}
+	return b;
+}
---
+++ examples/hom_2nd_order/run.sh
@@ -0,0 +1,1 @@
+./../../bin/milu -v -k --save-killing-result -u --exec-strategy=mut-1st-full --order=2 -h -c 'gcc -lm' -t 'gt_test.c' -d 'milu_driver.c' gt.c
